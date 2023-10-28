1420945dc4642609495c998b61647280b665ea91
ᴍʀ.ᴘʀᴏɢᴀᴍᴍᴇʀ <109902449+MrProgrammer72@users.noreply.github.com>
2023-10-28 13:47:50 +0530

    Update help.py

--- AnonXMusic/utils/inline/help.py
+++ AnonXMusic/utils/inline/help.py
@@ -110,7 +110,7 @@ def private_help_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="ʜᴇʟᴘ 💊"
+                text="ʜᴇʟᴘ 💊",
                 url=f"https://t.me/{app.username}?start=help",
             ),
                         InlineKeyboardButton( 
